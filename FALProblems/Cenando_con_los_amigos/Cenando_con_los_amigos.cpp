42d3e0a9fa14d348ce2837af387a154b71211b3a
insus100 <inigovelasco@gmail.com>
2022-10-14 01:01:54 +0200

    cenando con los amigos start

---
+++ FALProblems/Cenando_con_los_amigos/Cenando_con_los_amigos.cpp
@@ -0,0 +1,62 @@
+// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
+struct Sol {
+    int suma;
+    int ini;
+    int dias;
+};
+// función que resuelve el problema
+Sol resolver(vector<int> const &datos) {
+    
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int n;
+    cin >> n;
+    if (!std::cin)
+        return false;
+    vector<int> pesos(n);
+    for (int i = 0; i < n; i++) {
+        cin >> pesos[i];
+    }
+
+    Sol sol = resolver(pesos);
+
+    // escribir sol
+
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}

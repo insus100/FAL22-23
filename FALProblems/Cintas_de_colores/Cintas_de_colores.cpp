4a566780e7a82b872439d866fd024350e990e272
insus100 <inigovelasco@gmail.com>
2022-11-08 20:38:31 +0100

    cintas de colores

--- FALProblems/Cintas_de_colores/Cintas_de_colores.cpp
+++ FALProblems/Cintas_de_colores/Cintas_de_colores.cpp
@@ -6,39 +6,63 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct Cinta {
     char color;
     int longitud;
-    bool operator<(Cinta const& b) const {//comparador de cintas: azul < verde < rojo
+    /*bool operator<(Cinta const& b) const {//comparador de cintas: azul < verde < rojo
         if (color == 'a' && b.color == 'v' || color == 'a' && b.color == 'r' || color == 'v' && b.color == 'r') {
             return true;
         }
         return false;
+    }*/
+    bool operator<=(Cinta const& b) const {
+        return longitud <= b.longitud;
     }
 };
+
+bool ordenarPorLongitud(Cinta const& a, Cinta const& b) {
+    return a.longitud <= b.longitud;
+}
 
 struct Sol {//dónde empiezan y terminan las cintas verdes
     int ini;
     int fin;
 };
-void increment(int& index, int const& max) {
-    if (index + 1 < max) index++;
+void intercambiar(vector<Cinta>& v, int const& a, int const& b) {//a y b son indices
+    Cinta aux = v[b];
+    v[b] = v[a];
+    v[a] = aux;
 }
 // función que resuelve el problema
-Sol resolver(vector<Cinta> &v, int const &n) {
-    int i = 0, j = 1;
-    Sol sol{ -1, -1 };
-    while (i < n && j < n) {
-        if (v[i] < v[j]) increment(j, n);
-        else if(v[j] < v[i]) {//cambiamos v[i] por v[j]
-            Cinta aux = v[i];
-            v[i] = v[j];
-            v[j] = aux;
-            increment(i, n);
-        } else increment(j, n);
+Sol resolver(vector<Cinta> &v, int n) {
+    int i = 0, j = 0;
+    Sol sol{ 0, 0 };
+    while (j != n) {
+        switch (v[j].color)
+        {
+        case 'a': {
+            intercambiar(v, j, i);
+            i++;
+            j++;
+            break;
+        }
+        case 'v': {
+            j++;
+            break;
+        }
+        case 'r': {
+            intercambiar(v, j, n - 1);
+            n--;
+            break;
+        }
+        default:
+            break;
+        }
     }
+    sol.ini = i;
     return sol;
 }
 
@@ -51,20 +75,30 @@ bool resuelveCaso() {
     if (!std::cin)
         return false;
     vector<Cinta> v(n);
+    int verdes = 0;
     for (int i = 0; i < n; i++) {
         cin >> v[i].color >> v[i].longitud;
+        if (v[i].color == 'v')
+            verdes++;
     }
     Sol sol = resolver(v, n);
+    sol.fin = sol.ini + verdes;
     // escribir sol
+    sort(v.begin(), v.begin() + sol.ini, ordenarPorLongitud);
     cout << "Azules: ";
     for (int i = 0; i < sol.ini; i++)
         cout << v[i].longitud << " ";
+
+    sort(v.begin() + sol.ini, v.begin() + sol.fin, ordenarPorLongitud);
     cout << "\nVerdes: ";
     for (int i = sol.ini; i < sol.fin; i++)
         cout << v[i].longitud << " ";
+
+    sort(v.begin() + sol.fin, v.begin() + n, ordenarPorLongitud);
     cout << "\nRojas: ";
     for (int i = sol.fin; i < v.size(); i++)
         cout << v[i].longitud << " ";
+    cout << "\n";
     return true;
 
 }
